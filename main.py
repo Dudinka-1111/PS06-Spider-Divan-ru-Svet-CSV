c1dca895ed1f2f86dba1c3ecffcfcc404b752e96
Dudinka-1111 <dudinka1@mail.ru>
2025-01-06 15:40:46 +0500

    add new code

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+url = "https://www.divan.ru/perm/category/lyustry"
+driver.get(url)
+time.sleep(3)
+
+lyustries = driver.find_elements(By.CLASS_NAME, 'LlPhw')
+
+parsed_data = []
+
+for lyustry in lyustries:
+    try:
+        name = lyustry.find_element(By.CSS_SELECTOR, 'span.name').text
+        price = lyustry.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        url = lyustry.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8').get_attribute('href')
+
+    except:
+        print("произошла ошибка при парсинге")
+        continue
+
+    parsed_data.append([name, price, url])
+
+driver.quit()
+
+with open("divan.csv", 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Название люстры', 'Цена', 'Ссылка'])
+    writer.writerows(parsed_data)
