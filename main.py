f69e92aabb0e8412be5ab3ece39f7b13b374d861
Dudinka-1111 <dudinka1@mail.ru>
2025-01-11 22:05:04 +0500

    webdriver.Chrome()

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 url = "https://www.divan.ru/perm/category/lyustry"
 driver.get(url)
 time.sleep(20)
@@ -15,7 +15,7 @@ parsed_data = []
 for lyustry in lyustries:
     try:
         name = lyustry.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
-        price = lyustry.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        price = lyustry.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU.KIkOH').text
         url = lyustry.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8').get_attribute('href')
 
     except:
